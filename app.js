090edcb3a92665873726845981cf81bf88856561
KhaowPadKai <paritipat@gmail.com>
2023-10-01 03:39:21 +0700

    Connect to MongoDB
    Connect to MongoDB by JSON

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
 const path = require('path');
-
+const mongoose = require('mongoose');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-
-const app = express();
-
-app.set('view engine', 'ejs');
-app.set('views','views');
-
-const notememo = require('./routes/notes');
-const homeRoutes = require('./routes/home');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(homeRoutes);
-app.use(notememo);
+mongoose.Promise = global.Promise;
 
 
-app.use((req,res,next)=>{
-    res.status(404).render('404',{docTitle:'404 Not Found',path:""});
-})
+mongoose.connect('mongodb+srv://paritipat:8HcLg4CyijIEQYwZ@cluster0.8nkq9qf.mongodb.net/?retryWrites=true&w=majority')
+  .then(() => {
+    console.log('Connected to MongoDB');
+    // เรียกใช้ฟังก์ชัน importData
+  })
+  .catch((err) => {
+    console.error(err);
+  });
 
-app.listen(3000);
+
+  const app = express();
+
+  app.set('view engine', 'ejs');
+  app.set('views','views');
+  
+  const notememo = require('./routes/notes');
+  const homeRoutes = require('./routes/home');
+  
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(express.static(path.join(__dirname, 'public')));
+  
+  app.use(homeRoutes);
+  app.use(notememo);
+  
+  
+  app.use((req,res,next)=>{
+      res.status(404).render('404',{docTitle:'404 Not Found',path:""});
+  })
+  
+  app.listen(3000);
